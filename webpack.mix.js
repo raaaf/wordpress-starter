8ce95ac67fd19ff767bb31ac0918502dd62d7c3b
Rafael <a@rafaelalex.de>
2025-02-22 18:36:50 +0100

    tailwindcss 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,39 +1,28 @@
-let mix = require('laravel-mix');
-let path = require('path');
+let mix = require("laravel-mix");
+let path = require("path");
 
-mix.setPublicPath(path.resolve('./'));
+mix.setPublicPath(path.resolve("./"));
 
-mix.js('resources/js/app.js', 'dist/');
+mix.js("resources/js/app.js", "dist/");
 
-mix.postCss("resources/css/app.css", "dist/app.css");
+mix.postCss("resources/css/app.css", "dist/app.css", [require("@tailwindcss/postcss")]);
 
-mix.postCss("resources/css/editor-style.css", "./");
+mix.postCss("resources/css/editor-style.css", "./", [require("@tailwindcss/postcss")]);
 
-mix.copyDirectory("resources/img", "./img")
+mix.copyDirectory("resources/img", "./img");
 
-mix.copyDirectory("resources/fonts", "./fonts")
+mix.copyDirectory("resources/fonts", "./fonts");
 
-mix.copyDirectory("resources/favicons", "./favicons")
+mix.copyDirectory("resources/favicons", "./favicons");
 
 mix.options({
-    processCssUrls: false,
-    postCss: [
-        require('postcss-nested-ancestors'),
-        require('postcss-nested'),
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ]
+  processCssUrls: false,
 });
 
 mix.browserSync({
-    files: [
-        'resources/**/*',
-        'templates/**/*',
-        'index.php',
-    ],
-    proxy: 'http://localhost:10048',
-    https: false
+  files: ["resources/**/*", "templates/**/*", "index.php"],
+  proxy: "LOCAL-URL GOES HERE",
+  https: false,
 });
 
 mix.version();
