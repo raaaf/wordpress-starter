b6f13375000fabe49ccf9fe7fd7c1c456b1737bb
Rafael <a@rafaelalex.de>
2025-02-22 20:03:24 +0100

    Next improvements

--- templates/partials/header.blade.php
+++ templates/partials/header.blade.php
@@ -1,38 +1,54 @@
 <!DOCTYPE html>
-<html @php( language_attributes() ) class="no-js">
+<html @php(language_attributes()) class="no-js">
+
 <head>
-    <meta charset="@php( bloginfo( 'charset' ))">
+    <meta charset="@php(bloginfo('charset'))">
     <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Security-Policy"
+        content="default-src 'self'; script-src 'self' 'unsafe-inline' https://trusted.cdn.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="{{ esc_url( get_bloginfo( 'pingback_url' )) }}">
+    <link rel="pingback" href="{{ esc_url(get_bloginfo('pingback_url')) }}">
 
-    <link rel="preload" href="{{ esc_url(get_stylesheet_directory_uri()) }}/dist/app.css" as="style">
-    <link rel="preload" href="{{ esc_url(get_stylesheet_directory_uri()) }}/dist/app.js" as="script">
+    {{-- Preload critical assets --}}
+    <link rel="preload" href="{{ theme_get_mix_asset('dist/app.css') }}" as="style">
+    <link rel="preload" href="{{ theme_get_mix_asset('dist/app.js') }}" as="script">
 
-    {{-- create your favicon-package here: https://realfavicongenerator.net/ --}}
-    {{-- Uncomment and update these favicon links when ready for production
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/favicon-16x16.png">
-    <link rel="manifest" href="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/site.webmanifest">
-    <link rel="mask-icon" href="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/safari-pinned-tab.svg" color="#15b48d">
-    <link rel="shortcut icon" href="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#00aba9">
-    <meta name="msapplication-config" content="{{ esc_url(get_stylesheet_directory_uri()) }}/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
-    --}}
+    {{-- Inline critical CSS --}}
+    <style>
+        {{ file_get_contents(get_stylesheet_directory() . '/dist/critical.css') }}
+    </style>
 
-    {{-- Remove no-js class to indicate JavaScript is enabled --}}
+    {{-- Remove no-js class when JS is enabled --}}
     <script>
         document.documentElement.classList.remove('no-js');
     </script>
 
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "{{ get_bloginfo('name') }}",
+        "url": "{{ home_url() }}",
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "{{ home_url() }}/?s={search_term_string}",
+            "query-input": "required name=search_term_string"
+        }
+    }
+    </script>
+
     @php(wp_head())
 </head>
-<body @php(body_class( 'bg-white antialiased' ))>
+
+<body @php(body_class('bg-white antialiased'))>
 
     @php(wp_body_open())
 
-    <header class="p-4 md:p-8">
-        @include('partials.header-menu')
+    {{-- Skip Link for Accessibility --}}
+    <a href="#main-content" class="skip-link">Skip to content</a>
+
+    <header class="p-4 md:p-8" role="banner">
+        <nav role="navigation" aria-label="Main Navigation">
+            @include('partials.header-menu')
+        </nav>
     </header>
